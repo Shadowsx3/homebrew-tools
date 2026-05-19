0e4e1856ba65f3117fbc19c441097308c3b6ac69
Emma <41681538+Shadowsx3@users.noreply.github.com>
2026-05-19 09:49:38 -0300

    Use packaged libusbmuxd release tarball

--- Formula/libusbmuxd-wifi.rb
+++ Formula/libusbmuxd-wifi.rb
@@ -1,25 +1,25 @@
 class LibusbmuxdWifi < Formula
   desc "Patched usbmux client library for iPhone Wi-Fi discovery on macOS"
   homepage "https://github.com/Shadowsx3/libusbmuxd"
-  url "https://github.com/Shadowsx3/libusbmuxd/archive/refs/tags/v2.1.1-wifi.1.tar.gz"
-  version "2.1.1-wifi.1"
-  sha256 "8a14a2e8ca4299a6f18abc6b85423835717e035921063c7797d598e7d44e0fef"
+  url "https://github.com/Shadowsx3/libusbmuxd/releases/download/v2.1.1-wifi.3/libusbmuxd-2.1.1-wifi.3.tar.bz2"
+  version "2.1.1-wifi.3"
+  sha256 "c88946613634e930eafb0492451c302cda8888c5c1b2ff803a4a9e2c818969d6"
   license all_of: ["GPL-2.0-or-later", "LGPL-2.1-or-later"]
   head "https://github.com/Shadowsx3/libusbmuxd.git", branch: "master"
 
   keg_only "it installs libusbmuxd files that conflict with Homebrew's stock libusbmuxd"
 
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
+  depends_on "autoconf" => :build if build.head?
+  depends_on "automake" => :build if build.head?
+  depends_on "libtool" => :build if build.head?
   depends_on "pkgconf" => :build
 
   depends_on "libimobiledevice-glue"
   depends_on "libplist"
 
   def install
-    (buildpath/".tarball-version").write version
-    system "./autogen.sh", "--disable-silent-rules", *std_configure_args
+    configure = build.head? ? "./autogen.sh" : "./configure"
+    system configure, "--disable-silent-rules", *std_configure_args
     system "make", "install"
   end
 
